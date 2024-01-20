12d970200defa4acfbd137d60cc4c8fb70b749fb
Michael Reginiano <mike.reginiano@gmail.com>
2024-01-20 12:37:01 +0100

    set number of simulations to 50k

--- European_Option.py
+++ European_Option.py
@@ -7,13 +7,12 @@ Created on Fri Jan 19 14:14:46 2024
 """
 import numpy as np
 import math
-import matplotlib.pyplot as plt
 
 S_0 = 100 #starting price
 mu = 0.1 #asset drift
 sig = 0.3 #volatility
 dt = 1/365 #time step
-num_paths = 5 #number of price paths to simulate
+num_paths = 50000 #number of price paths to simulate
 
 E = 110 #Option strike
 interest_rate = 0.03 #risk-free interest rate
@@ -24,18 +23,6 @@ dW_matrix = np.random.normal(0,math.sqrt(dt),size=(num_paths,365))
 
 #Compute price paths using vectorisation
 price_paths = S_0 * np.exp(np.cumsum(mu*dt+sig*dW_matrix,axis=1))
-
-fig,ax = plt.subplots()
-i = 1
-for p in price_paths:
-    plt.plot(p, label= f'Path {i}')
-    i = i+1
-    
-plt.title('Geometric Brownian Motion of Asset Price Paths')
-plt.xlabel('Time (days)')
-plt.ylabel('Price ($)')
-plt.legend()
-plt.show()
 
 def European_Call_Payoff(S,E):
     return max(S-E,0)
